8f7c4bdba22e4748bd090130f72b6b3dc96f3044
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2017-03-06 18:29:42 +0300

    Use `sharp` for higher quality resizing

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const Promise = require('bluebird')
 const path    = require('path')
 const exec    = require('child_process').exec
 const fs      = require('fs-extra-promise')
+const sharp   = require('sharp')
 
 
 
-const sizes     = [..._.range(10, 32), 32, 48, 64, 128] // MUST include 128
-const tmpPath   = 'tmp'
-const distPath  = 'dist'
-const styleName = 'style.scss'
+const sizes         = [..._.range(10, 32), 32, 48, 64, 128]
+// const sizes         = [20]
+const srcPath       = 'bower_components/emojione/assets/png_512x512'
+const tmpPath       = 'tmp'
+const tmpResizePath = path.join(tmpPath, 'resize')
+const tmpSpritePath = path.join(tmpPath, 'sprite')
+const distPath      = 'dist'
+const styleName     = 'style.scss'
 
 
 
@@ -19,6 +24,7 @@ const promisify =
     new Promise ((resolve, reject) => {
       callback(err => {
         if (err) {
+          console.log('Error:', err.message, err)
           reject(...arguments)
         } else {
           resolve(...arguments)
@@ -38,65 +44,77 @@ const execPrm =
       //   console.log(`stderr: ${stderr}`);
       // })
 
+console.log("Starting.")
 
+sizes
+  .reduce((promise, size) => {
+    return promise
+      .then(() => fs.ensureDirAsync(tmpResizePath))
+      .then(() => fs.ensureDirAsync(tmpSpritePath))
 
-console.log("Starting. Generating sprites with sprity.")
-console.log("This is gonna take a while, have a coffee.")
-
-promisify(
-  callback =>
-    sprite
-      .create({
-        src: 'bower_components/emojione/assets/png_128x128/*.png',
-        out: tmpPath,
-        style: styleName,
-        processor: 'emojione-scss',
-        margin: 0,
-        // 'no-sort': true,
-        prefix: 'emojione',
-        dimension:
-          sizes
-            .map(size => ({ratio: size, dpi: size * 10}))
-        },
-        callback
-      )
-)
-
-  .then(() => {
-    console.log("Finished generating sprites. Optimizing with pngcrush.")
-    return fs.ensureDirAsync(distPath)
-  })
-
-  .then(() =>
-    Promise.all(
-      sizes.map(size => {
-        const sourcePath = path.join(tmpPath, `sprite@${size}x.png`)
-        const targetPath = path.join(distPath, `sprite-${size}.png`)
-        const command    = `pngcrush -rem alla -nofilecheck -reduce -m 7 ${sourcePath} ${targetPath}`
-
-        // https://zoompf.com/blog/2014/11/png-optimization
-        return execPrm(command)
-          .then(() => console.log("-- Finished", command))
-          .catch(() => {
-            console.log("-- Failed", command)
-            return Promise.reject(...arguments)
-          })
+      .then(() => {
+        console.log(`Resizing emoji to size ${size}`)
+        return fs.readdirAsync(srcPath)
       })
-    )
-  )
+
+      .then(fileNames =>
+        Promise.all(
+          fileNames.map(fileName => {
+            const inputFileName  = path.join(srcPath, fileName)
+            const outputFileName = path.join(tmpResizePath, fileName)
+            return promisify(callback =>
+              sharp(inputFileName)
+                .resize(size, size)
+                .toFile(outputFileName, callback)
+            )
+          })
+        )
+      )
+
+      .then(() => {
+        console.log(`Generating spritesheet of size ${size}`)
+        return promisify(
+          callback =>
+            sprite
+              .create({
+                  src:       path.join(tmpResizePath, '*.png'),
+                  out:       tmpSpritePath,
+                  name:      `sprite-${size}`,
+                  style:     styleName,
+                  processor: 'emojione-scss',
+                  margin:    0,
+                  prefix:    'emojione',
+                  dimension: [ { ratio: 1, dpi: 1 } ],
+                },
+                callback
+              )
+        )
+      })
+
+    .then(() => {
+      // https://zoompf.com/blog/2014/11/png-optimization
+      console.log(`Optimizing spritesheet of size ${size}`)
+
+      const sourcePath = path.join(tmpSpritePath, `sprite-${size}.png`)
+      const targetPath = path.join(distPath,      `sprite-${size}.png`)
+      const command    = `pngcrush -rem alla -nofilecheck -reduce -m 7 ${sourcePath} ${targetPath}`
+
+      return execPrm(command)
+    })
+  }, Promise.resolve())
 
   .then(() => {
-    console.log("Finished optimizing sprites. Moving style file to dist.")
+    console.log("Finished generating & optimizing sprites. Moving style file to dist.")
 
-    const sourcePath = path.join(tmpPath,  styleName)
-    const targetPath = path.join(distPath, styleName)
+    const sourcePath = path.join(tmpSpritePath, styleName)
+    const targetPath = path.join(distPath,      styleName)
     return fs.renameAsync(sourcePath, targetPath)
   })
 
   .then(() => {
     if (tmpPath.slice(0, 1) === '/') return
     console.log("Removing tmp folder.")
-    fs.removeAsync(tmpPath)
+    return fs.removeAsync(tmpPath)
   })
 
   .then(() => {
